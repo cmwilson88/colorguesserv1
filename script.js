a0335ec7d32fe5a6fcb9af62d49a89d1aa7a8036
Christopher Wilson <cmw.wilson88@gmail.com>
2016-12-13 22:01:52 -0500

    Add functionality to check for win and winning display

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ var gameOptions = document.getElementsByClassName('gameOption');
 var hardRow = document.querySelector('.hardRow');
 var difficulty = document.querySelector('#difficulty');
 var displayGoal = document.querySelector('.displayGoal');
+var header = document.querySelector(".header1");
 var winningColor;
 var easy = false;
+
+var test = document.querySelector("#test");
 
 function randRGBValue() {
   return Math.floor(Math.random()*255);
@@ -17,16 +20,35 @@ function randIndex(arr) {
   return Math.floor(Math.random()*arr.length);
 }
 
+
 function generateRGB() {
   var color = 'rgb(';
-  color += randRGBValue() + ',';
-  color += randRGBValue() + ',';
+  color += randRGBValue() + ', ';
+  color += randRGBValue() + ', ';
   color += randRGBValue() + ')';
   return color;
 }
 
+function visible() {
+	for (var i=0; i< gameOptions.length; i++){
+		if (gameOptions[i].classList.contains('invisible')) {
+			gameOptions[i].classList.remove('invisible');
+		}
+	}
+}
+
+function displayWin() {
+	visible();
+	for (var i = 0; i < gameOptions.length; i++) {
+		gameOptions[i].style.backgroundColor = winningColor;
+	}
+	header.style.backgroundColor = winningColor;
+
+}
+
 function fillColors() {
   colors = [];
+  visible();
   for (var i = 0; i < gameOptions.length; i++) {
     var color = generateRGB();
     gameOptions[i].style.background = color;
@@ -36,9 +58,22 @@ function fillColors() {
   displayGoal.textContent = winningColor;
 }
 
-function checkClicked() {
-	console.log("clicked!");
+function guessColor (){
+    for (var i=0;i< gameOptions.length;i++){
+        gameOptions[i].addEventListener("click", function(){
+        	console.log(this.style.backgroundColor);
+        	if (this.style.backgroundColor === winningColor) {
+        		console.log("You Win");
+        		displayWin();
+        	} else {
+        		console.log("Try again");
+        		this.classList.add("invisible");
+        	}
+        })
+    }
 }
+
+
 
   
 resetButton.addEventListener("click", fillColors);
@@ -59,5 +94,5 @@ hardButton.addEventListener("click", function(){
 });
 
 
-
 fillColors();
+guessColor();
